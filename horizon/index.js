3468c9475962907266e4a458aaec9a8c2fdbed6e
finepung <j.unger@hbk-bs.de>
2025-02-27 13:13:54 +0100

    jdend

--- horizon/index.js
+++ horizon/index.js
@@ -1,30 +1,18 @@
 // this is a comment
-//. this is global scope
+
 function setup() {
 	// this is function scope
 	// setup is executed once
-
-	createCanvas(100, 100);
-	const x = (n) => width * n;
-	const y = (n) => height * n;
-	background("lightgray");
-
-	//first layer
+	createCanvas(200, 100);
+	background("pink");
+	//sun
+	fill("lightyellow");
 	noStroke();
-	fill("darkgray");
-	rectMode(CORNER);
-	rect(x(0.15), y(0.15), 120, 120);
-
-	//second layer
+	circle(100, 55, 40);
+	//water
+	fill("lightblue");
 	noStroke();
-	fill("gray");
-	rectMode(CORNER);
-	rect(x(0.3), y(0.3), 80, 80);
-
-	//third layer
-	fill("black");
-	rectMode(CORNER);
-	rect(x(0.45), y(0.45), 60, 60);
+	rect(0, 57, 200, 43);
 }
 
 function keyPressed() {
